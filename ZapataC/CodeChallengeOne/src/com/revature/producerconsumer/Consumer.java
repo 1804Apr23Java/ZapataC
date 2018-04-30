5afeb1a7f5b7e100c0e64a93cc6052c0e29b277d
Chris Zapata <czapatarun@gmail.com>
2018-04-30 11:17:22 -0400

    getting rid of a few useless lines

--- ZapataC/CodeChallengeOne/src/com/revature/producerconsumer/Consumer.java
+++ ZapataC/CodeChallengeOne/src/com/revature/producerconsumer/Consumer.java
@@ -2,18 +2,16 @@ package com.revature.producerconsumer;
 
 public class Consumer extends Thread {
 	private Basket basket;
-	private int number;
 	
 	public Consumer (Basket b, int number) {
 		basket = b;
-		this.number = number;
 	}
 	
 	public void run() {
 		int value = 0;
 		for (int i = 0; i < 10; i++) {
 			value = basket.get();
-			System.out.println("Consumer #" + this.number + " got: " + value);
+			System.out.println("Consumer got: " + value);
 		}
 	}
 }
