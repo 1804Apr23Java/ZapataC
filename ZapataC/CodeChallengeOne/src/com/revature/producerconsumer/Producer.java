b0926de957c40751092d240c33fbc0330661c04f
Chris Zapata <czapatarun@gmail.com>
2018-04-30 11:17:49 -0400

    Getting rid of a few useless lines in this one too.

--- ZapataC/CodeChallengeOne/src/com/revature/producerconsumer/Producer.java
+++ ZapataC/CodeChallengeOne/src/com/revature/producerconsumer/Producer.java
@@ -2,18 +2,16 @@ package com.revature.producerconsumer;
 
 public class Producer extends Thread {
 	private Basket basket;
-	private int number;
 	private final int capacity = 10;
 	
 	public Producer (Basket b, int number) {
 		basket = b;
-		this.number = number;
 	}
 	
 	public void run() {
 		for (int i = 0; i < capacity; i++) {
 			basket.put(i);
-			System.out.println("Producer #" + this.number + " put: " + i);
+			System.out.println("Producer put: " + i);
 			try {
 				sleep ((int)(Math.random() * 100));
 			} catch (InterruptedException e) {
